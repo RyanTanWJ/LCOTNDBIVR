27eff6dc8a2125e488f76e9e8457faa3b7dbf344
Matthew Chan <_mattc@hotmail.com>
2018-09-02 10:03:32 +0800

    Spawn enemies at proper positions based on a cone

--- VRTK-3.2.1/Assets/Scripts/EnemyManager.cs
+++ VRTK-3.2.1/Assets/Scripts/EnemyManager.cs
@@ -8,18 +8,37 @@ public class EnemyManager : MonoBehaviour {
     private GameObject enemyPrefab;
 
     [SerializeField]
-    private float minX, maxX, spawnY, minZ, maxZ;
+    private float columnCount, rowCount, arenaIncline, tileSpacing;
+
+    private int[,] enemyGrid;
+    private Transform enemyHolder;
+
+    void Start() {
+        enemyGrid = new int[(int)columnCount, (int)rowCount];
+        enemyHolder = new GameObject("Enemies").transform;
+    }
 
     private void OnSpawnCommand() {
-        float spawnX = Random.Range(minX, maxX);
-        float spawnZ = Random.Range(minZ, maxZ);
+        //Spawn on the outermost row
+        int spawnColumnPosition = (int)columnCount - 1;
+        int spawnRowPosition = Random.Range((int)0, (int)rowCount);
+
+        float angle = 2 * Mathf.PI / columnCount;
+        float radius = spawnColumnPosition * (spawnColumnPosition + 1);
+        float inclineRad = arenaIncline * Mathf.Deg2Rad;
+
+        float spawnX = radius * Mathf.Cos(angle);
+        float spawnZ = radius * Mathf.Sin(angle);
+        float spawnY = radius * Mathf.Tan(inclineRad);
 
         Vector3 spawnPosition = new Vector3(spawnX, spawnY, spawnZ);
+        Quaternion spawnRotation = Quaternion.LookRotation(-spawnPosition);
 
-        Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+        Instantiate(enemyPrefab, spawnPosition, spawnRotation, enemyHolder);
     }
 
     public void SpawnEnemy() {
         OnSpawnCommand();
     }
+
 }
