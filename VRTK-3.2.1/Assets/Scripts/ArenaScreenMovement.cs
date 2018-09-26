c854712d852bd5f5b9141f9861e0ae7259ea64c0
Matthew Chan <_mattc@hotmail.com>
2018-09-26 14:30:18 +0800

    Arena Screen Attempt

---
+++ VRTK-3.2.1/Assets/Scripts/ArenaScreenMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArenaScreenMovement : MonoBehaviour {
+
+    [SerializeField]
+    GameObject cameraVR;
+    float distance = 15;
+    float height = 10;
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        Vector3 newPosition = cameraVR.transform.forward.normalized * distance + new Vector3(0, height, 0);
+        Quaternion newRotation = Quaternion.LookRotation(-newPosition);
+
+        transform.SetPositionAndRotation(newPosition, newRotation);
+    }
+}
