bd7fba6f4b5e5b7b5f90fe790f24a4562617e9df
Matthew Chan <_mattc@hotmail.com>
2018-09-26 12:13:08 +0800

    Score should now start displaying from zero on game over

--- VRTK-3.2.1/Assets/Scripts/MenuController.cs
+++ VRTK-3.2.1/Assets/Scripts/MenuController.cs
@@ -10,11 +10,27 @@ public class MenuController : MonoBehaviour {
     GameObject RetryTarget;
     [SerializeField]
     TMPro.TextMeshPro Score;
+    [SerializeField]
+    AudioSource countSource;
 
     public void GameOverMenu(int score)
     {
         StartTarget.SetActive(false);
         RetryTarget.SetActive(true);
-        Score.text = "" + score;
+        //Score.text = "" + score;
+        StartCoroutine(IncreaseScore(score));
+    }
+
+    IEnumerator IncreaseScore(int score)
+    {
+        int displayScore = 0;
+        while (displayScore < score)
+        {
+            displayScore = Mathf.Min(displayScore + 200, score);
+            Score.text = "" + displayScore;
+            countSource.Play();
+            yield return null;
+        }
+        yield return null;
     }
 }
