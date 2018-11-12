cc139df04902ab3525e992ff60bc5507e8742f30
Matthew Chan <_mattc@hotmail.com>
2018-11-12 15:25:51 +0800

    Beating Flow Meter Untested

--- VRTK-3.2.1/Assets/Scripts/FlowTextureController.cs
+++ VRTK-3.2.1/Assets/Scripts/FlowTextureController.cs
@@ -13,35 +13,49 @@ public class FlowTextureController : MonoBehaviour {
     private Player player;
     private Renderer renderMaterial;
 
-    private void OnEnable()
-    {
+	private RhythmController rhythmController;
+	private float rhythmState = 0;
+
+    private void OnEnable() {
         RhythmController.BeatTriggeredEvent += UpdatePattern;
     }
 
-    private void OnDisable()
-    {
+    private void OnDisable() {
         RhythmController.BeatTriggeredEvent -= UpdatePattern;
     }
 
-    void Start()
-    {
+    void Start() {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
         renderMaterial = GetComponent<Renderer>();
+		rhythmController = GameObject.FindGameObjectWithTag("RhythmController").GetComponent<RhythmController>();
     }
 
-    private void UpdatePattern()
-    {
-        if (player.Health < onThreshold)
-        {
+	private void Update() {
+		rhythmState = rhythmController.GetCurrentBeat();
+		renderMaterial.material.color = beatToColor(rhythmState);
+	}
 
+	private void UpdatePattern() {
+        if (player.Health < onThreshold) {
             renderMaterial.material = materialOff;
-
-        }
-        else
-        {
-
-            renderMaterial.material = materialOn;
-
+        } else {
+			renderMaterial.material = materialOn;
         }
     }
+
+	private Color beatToColor(float beat) {
+		//Base Color is (0.5, 0.5, 0.5)
+		//Scales to/from (1,1,1) between 0.9-1 / 0-0.1
+		float temp = 0.5f;
+		if (beat <= 0.9 || beat >= 0.1) {
+			return new Color(0.5f, 0.5f, 0.5f);
+		} else if (beat > 0.9f) {
+			temp += ((beat - 0.9f) * 5f);
+		} else {
+			temp += (0.5f - (beat * 5f));
+		}
+
+		return new Color(temp, temp, temp);
+	}
+
 }
